e6d8adfe2f5b9d1d14885cc245036e21df647eb3
k022g1042 <115126459+k022g1042@users.noreply.github.com>
2023-04-28 11:08:08 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow(L"GC2C_06_カラサワカズマ");
+	win->CreateGameWindow(L"GC2D_06_カラサワカズマ_AL2");
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
